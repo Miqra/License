2c4c66b5ccf79ff2f7efcfb8443a5fbaafb28b88
Nigar <Nigar@NIGAR-PC>
2016-10-13 15:21:29 +0400

    MiqraplusLicense

---
+++ WebApplication2/WebApplication2/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+namespace WebApplication2.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string name, string password)
+        {
+            if ("admin".Equals(name) && "123".Equals(password))
+            {
+                Session["user"] = new User() { Login = name, Name = "Nigar Hacibabayeva" };
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
